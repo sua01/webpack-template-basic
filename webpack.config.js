5b94e12e506e88d09b807658e5a7550c8ef1da71
sua01 <sua0211@naver.com>
2022-10-19 11:12:55 +0900

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// import
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+// export 
+module.exports = {
+  entry: './js/main.js',  // 파일을 읽어들이기 시작한느 진입점 설정
+
+  output: { // 결과물(번들)을 반환하는 설정
+    // path: path.resolve(__dirname, 'dist'), // 절대경로 명시해야함 / dirname는 현재 파일이 있는 실제 경로
+    // filename: 'main.js',
+    clean: true // 기존에 만들었던 내용 제거해줌
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  plugins: [  // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
+    new HtmlPlugin({
+      template: './index.html'
+    }),
+
+    new CopyPlugin({
+      patterns: [
+        { from: 'static' }  // static 폴더 안에 있는 파일들이 복사되어서 dist라는 폴더로 들어갈 수 있게 해줌
+      ]
+    })
+  ],
+
+  devServer: {
+    host: 'localhost'
+  }
+}
